7a616032436df0ca930d10c6ff728496439df613
Arturo Barreto Villaf√°n <Barreto@arturos-macbook-pro.local>
2015-11-03 09:46:07 -0600

    Initial Commit

---
+++ MiniReto/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MiniReto
+//
+//  Created by Arturo Barreto Villafán on 11/3/15.
+//  Copyright © 2015 Arturo Barreto Villafán. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
