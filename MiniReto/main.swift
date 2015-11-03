05c711bbc87b84b639886e2031dd00f7f084e303
Arturo Barreto Villaf√°n <Barreto@arturos-macbook-pro.local>
2015-11-03 09:52:28 -0600

    primer

--- MiniReto/main.swift
+++ MiniReto/main.swift
@@ -8,5 +8,47 @@
 
 import Foundation
 
-print("Hello, World!")
+var serie = 0...100
+var pares:[Int] = []
+var impares:[Int] = []
+var divisible:[Int] = []
+var rango:[Int] = []
+for numero in serie{
+    
+    if numero % 5 == 0{
+        divisible.append(numero)
+        
+    }
+        
+    else if numero % 2 == 0{
+        pares.append(numero)
+    }
+    else if numero % 2 == 1{
+        impares.append(numero)
+    }
+    
+    switch numero{
+        
+        
+    case 30...40:
+        rango.append(numero)
+        break;
+    default:
+        break;
+    }
+    
+    
+}
 
+for i in pares{
+    print("\(i) #Par")
+}
+for j in impares{
+    print("\(j) #Impar")
+}
+for k in divisible{
+    print("\(k) #Bingo")
+}
+for l in rango{
+    print("\(l) #Viva Swift")
+}
